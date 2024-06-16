d61feff1ce9a18b69462afec3366d7b29ae295f1
mahi <mahidhardevisetty@gmail.com>
2024-06-16 16:01:53 +0530

    as

--- zin.go
+++ zin.go
@@ -4,12 +4,12 @@ import "github.com/devisettymahidhar315/zin/api"
 
 func Zin() {
 
-	api.InitializeRoutes()
+	// api.InitializeRoutes()
 
-	// // Initialize the Gin router and routes
-	// Zin := api.InitializeRoutes()
+	// Initialize the Gin router and routes
+	Zin := api.InitializeRoutes()
 
-	// // Start the server
-	// Zin.Run()
+	// Start the server
+	Zin.Run()
 
 }
